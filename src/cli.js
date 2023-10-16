65fb223451b4e7c5c69b31f3b3404ee6ef84364a
ianarodrigues <moniquesrlima@gmail.com>
2023-10-16 17:57:21 -0300

    implements link validation

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,16 @@
 #!/usr/bin/env  node
+// const chalk = require('chalk');
+// console.log(chalk.red('Hello world!'));
+
+const { extractLinks } = require('./index');
+
+const filePath = process.argv[2];
+// console.log('Caminho do arquivo:', filePath);
+const options = {
+  validate: process.argv.includes('--validate'),
+  status: process.argv.includes('--status'),
+};
+
+extractLinks(filePath, options).then((links) => {
+  console.log(links);
+});
--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-// const chalk = require('chalk');
 const fs = require('fs');
 
-fs.readFile('README.md', 'utf8', (err, data) => {
-  if (err) throw err;
+function extractLinks(filePath, options) {
+  return fs.promises.readFile(filePath, 'utf8').then((data) => {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captures = [...data.matchAll(regex)];
+    const links = captures.map((capture) => ({
+      text: capture[1],
+      url: capture[2],
+      file: filePath,
+    }));
 
-  console.log(data);
-});
+    if (options.validate) {
+      const validations = links.map((link) =>
+        validateLinks(link));
+      return Promise.all(validations);
+    }
+    return links;
+  });
+}
 
-// function soma(a, b) {
-//   return a + b;
-// }
-// console.log(soma(3, 8));
+function validateLinks(link) {
+  return fetch(link.url)
+    .then((response) => {
+      if (response.ok) {
+        link.valid = true;
+        link.status = response.status;
+      } else {
+        link.valid = false;
+        link.status = response.status;
+      }
+      return link;
+    })
+    .catch((error) => {
+      link.valid = false;
+      link.error = error.message;
+      return link;
+    });
+}
 
-// console.log(chalk.red('Hello world!'));
+module.exports = { extractLinks, validateLinks };
