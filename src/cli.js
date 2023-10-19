2ab54a015f18a3385a745608070c4424fc7eb659
ianarodrigues <moniquesrlima@gmail.com>
2023-10-19 20:13:31 -0300

    add statistical calculations

--- src/cli.js
+++ src/cli.js
@@ -2,16 +2,15 @@
 // const chalk = require('chalk');
 // console.log(chalk.red('Hello world!'));
 
-const { extractLinks } = require('./index');
+const { extractLinks } = require("./index");
 
 const filePath = process.argv[2];
 // console.log('Caminho do arquivo:', filePath);
 const options = {
-  validate: process.argv.includes('--validate'),
-  status: process.argv.includes('--status'),
+  validate: process.argv.includes("--validate"),
+  stats: process.argv.includes("--stats"),
 };
 
 extractLinks(filePath, options).then((links) => {
   console.log(links);
 });
-
