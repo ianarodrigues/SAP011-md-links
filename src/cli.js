94319a6baf0e05b16a75a94d17238dd5cf4dbbc2
ianarodrigues <moniquesrlima@gmail.com>
2023-10-19 20:47:00 -0300

    chalk implementation

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,54 @@
 #!/usr/bin/env  node
-// const chalk = require('chalk');
 // console.log(chalk.red('Hello world!'));
 
-const { extractLinks } = require("./index");
+const chalk = require('chalk');
+const { extractLinks } = require('./index');
 
 const filePath = process.argv[2];
-// console.log('Caminho do arquivo:', filePath);
 const options = {
-  validate: process.argv.includes("--validate"),
-  stats: process.argv.includes("--stats"),
+  validate: process.argv.includes('--validate'),
+  stats: process.argv.includes('--stats'),
 };
 
-extractLinks(filePath, options).then((links) => {
-  console.log(links);
-});
+extractLinks(filePath, options)
+  .then((objLinks) => {
+    if (options.validate && !options.stats) {
+      objLinks.forEach((link) => {
+        if (link.status === 200) {
+          console.log(
+            chalk.bgGreen(`☑ OK ${link.status}`),
+            chalk.black('| ') + chalk.magenta(link.text),
+            chalk.white(link.file),
+            chalk.green(link.url),
+          );
+        } else {
+          console.log(
+            chalk.bgRed(`☒ FAIL ${link.status}`),
+            chalk.black('| ') + chalk.magenta(link.text),
+            chalk.white(link.file),
+            chalk.red(link.url),
+          );
+        }
+        console.log(
+          chalk.black('---------------------------------------------------------------------------------------------------------------------------------------')
+        );
+      });
+    } else if (options.stats && !options.validate) {
+      console.log(chalk.cyan(`Total: ${objLinks.total}`));
+      console.log(chalk.yellow(`Unique: ${objLinks.unique}`));
+    } else if (options.stats && options.validate) {
+      console.log(chalk.cyan(`Total: ${objLinks.total}`));
+      console.log(chalk.yellow(`Unique: ${objLinks.unique}`));
+      console.log(chalk.red(`Broken: ${chalk.red(objLinks.broken)}`));
+    } else {
+      objLinks.forEach((link) => {
+        console.log(chalk.white('Title: ') + chalk.cyan(link.text));
+        console.log(chalk.white('URL: ') + chalk.magenta(link.url));
+        console.log(chalk.white('File: ') + chalk.yellow(link.file));
+        console.log(
+          chalk.black('-------------------------------------------------------------------------------------------------------')
+        );
+      });
+    }
+  })
+ 
